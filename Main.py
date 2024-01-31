b0f4b2782e18328898b4af426487c1d52e0d1272
LuisWilke <wilke_lopes@hotmail.com>
2024-01-31 00:54:56 -0400

    :recycle: refactor

--- Main.py
+++ Main.py
@@ -1,25 +1,8 @@
-from flask import Flask, url_for, render_template
+from flask import Flask
+from routes.home import home_route
 
 # Initialize
 app = Flask(__name__)
-
-# routes
-@app.route('/')
-def ola_mundo():
-    titulo = "Gestão de usuários"
-    usuarios = [
-        {"nome": "Guilherme", "membro_ativo": True},
-        {"nome": "Joao", "membro_ativo": False},
-        {"nome": "Luis", "membro_ativo": False},
-    ]
-    return render_template("index.html", titulo=titulo, usuarios=usuarios)
-
-@app.route('/sobre')
-def pagina_sobre():
-    return """
-    <b>ProgramadorPython<b>: asssista os videos no 
-    <a href="https://youtube.com/@programadorpython">Canal no Youtube</a>
-    """
 
 
 # execution
